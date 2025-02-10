09939ce2c76f7327a471e2f9491d7cbea71fca09
yeticarus <153378072+yeticarus@users.noreply.github.com>
2025-02-10 15:57:28 +0100

    fix secondary entrance data extraction
    https://github.com/snesrev/smw/pull/43

--- app/jni/SMW_SRC/assets/compile_resources.py
+++ app/jni/SMW_SRC/assets/compile_resources.py
@@ -629,8 +629,8 @@ def print_all(args):
     add_asset_uint8('kLmLevelData3FE00', util.get_bytes(0x3FE00, 512) if lm_load_level else b'')    
 
     add_asset_uint8('kLmLevelData5DC85', util.get_bytes(get_24(0x5DC86), 512) if lm_load_level else b'')    
-    add_asset_uint8('kLmLevelData5DC8A', util.get_bytes(get_24(0x5DC8B), 512) if lm_load_level else util.get_bytes(0x5fe00, 0x200))    
-    add_asset_uint8('kLm5FE00', util.get_bytes(get_24(0x5DC81), 512) if lm_load_level else b'')    
+    add_asset_uint8('kLmLevelData5DC8A', util.get_bytes(get_24(0x5DC8B), 512) if lm_load_level else b'')    
+    add_asset_uint8('kLm5FE00', util.get_bytes(get_24(0x5DC81), 512) if lm_load_level else util.get_bytes(0x5fe00, 0x200))    
 
     add_asset_uint8('kLevelInfo_05F800', util.get_bytes(get_24(0xde191), 0x200) if lm_load_level else util.get_bytes(0x5f800, 0x200))
     add_asset_uint8('kLevelInfo_05FA00', util.get_bytes(get_24(0xde198), 0x200) if lm_load_level else util.get_bytes(0x5fa00, 0x200))
